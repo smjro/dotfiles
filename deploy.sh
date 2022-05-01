372925d0d647231b042eef87bcc0466aa28aae06
smjro <shimadataketo@smjro.local>
2022-05-02 08:51:41 +0900

    add files

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOTPATH="$(cd "$(dirname "$0")" && pwd)"
+
+for f in .??* ; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".DS_Store" ] && continue
+
+    ln -snfv "$DOTPATH/$f" "$HOME/$f"
+done
